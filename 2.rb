e703551e8d87d029bafdfd492dec3f41af513296
lsl <songlin.lin@yundata.com>
2018-04-01 20:40:10 +0800

    add 2.rb

---
+++ 2.rb
@@ -0,0 +1,19 @@
+if 1 == 1
+    puts "I'll be printed!"
+end
+
+if 1 > 2
+    puts "I won't!"
+end
+
+
+if true && true
+    puts "I'll be printed!"
+end
+
+number = 1
+
+while number < 10
+    puts "#{number}"
+    number += 1
+end
